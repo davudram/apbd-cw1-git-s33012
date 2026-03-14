2731769bf74ff5aa06990b10533d91310db2a32a
Davud Ramazanov <davudramazanov2007@gmail.com>
2026-03-15 00:26:41 +0100

    Add User class

---
+++ FirstProject/User.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstProject;
+
+public class User
+{
+    public User(int id, string name, string surname, string email, int age)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Surname = surname;
+        this.Email = email;
+        this.Age = age;
+    }
+    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+}
